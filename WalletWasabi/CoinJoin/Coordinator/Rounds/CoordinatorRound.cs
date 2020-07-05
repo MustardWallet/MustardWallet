38cb1e70987926caf8f22f97d3498b7ecbf9e0dd
Gabriel Krieger <gabrielkrieger@protonmail.com>
2020-07-05 23:01:28 +0000

    Enable Coordinator Fee Output again

--- WalletWasabi/CoinJoin/Coordinator/Rounds/CoordinatorRound.cs
+++ WalletWasabi/CoinJoin/Coordinator/Rounds/CoordinatorRound.cs
@@ -385,11 +385,14 @@ namespace WalletWasabi.CoinJoin.Coordinator.Rounds
 						}
 
 						// 6. Add Coordinator fee only if > about $3, else just let it to be miner fee.
-						/* This is disabled for Litecoin LTC because its price is not $10k at all ...
+						/* This check is disabled for Litecoin LTC because its price is not $10k at all ...
 						if (coordinatorFee > Money.Coins(0.0003m))
 						{
+						*/
 							transaction.Outputs.AddWithOptimize(coordinatorFee, coordinatorScript);
-						} */
+						/*
+						}
+						*/
 
 						// 7. Try optimize fees.
 						await TryOptimizeFeesAsync(transaction, spentCoins).ConfigureAwait(false);
