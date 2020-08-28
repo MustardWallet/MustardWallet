nopara73/WalletWasabi

Switch API from /btc/ to /ltc/
Thanks to Lucas Ontivero for this suggestion !

WalletWasabi/CoinJoin/Client/Clients/BobClient.cs
@@ -29,7 +29,7 @@ namespace WalletWasabi.CoinJoin.Client.Clients
 			Guard.NotNull(nameof(activeOutput), activeOutput);
 
 			var request = new OutputRequest { OutputAddress = activeOutput.Address, UnblindedSignature = activeOutput.Signature, Level = activeOutput.MixingLevel };
-			using var response = await TorClient.SendAsync(HttpMethod.Post, $"/api/v{Constants.BackendMajorVersion}/btc/chaumiancoinjoin/output?roundId={roundId}", request.ToHttpStringContent()).ConfigureAwait(false);
+			using var response = await TorClient.SendAsync(HttpMethod.Post, $"/api/v{Constants.BackendMajorVersion}/ltc/chaumiancoinjoin/output?roundId={roundId}", request.ToHttpStringContent()).ConfigureAwait(false);
 			if (response.StatusCode == HttpStatusCode.Conflict)
 			{
 				return false;

MustardWallet/MustardWallet

WalletWasabi/CoinJoin/Client/Clients/BobClient.cs
@@ -30,7 +30,7 @@ namespace WalletWasabi.CoinJoin.Client.Clients
 			Guard.NotNull(nameof(activeOutput), activeOutput);
 
 			var request = new OutputRequest { OutputAddress = activeOutput.Address, UnblindedSignature = activeOutput.Signature, Level = activeOutput.MixingLevel };
-			using var response = await TorClient.SendAsync(HttpMethod.Post, $"/api/v{WasabiClient.ApiVersion}/btc/chaumiancoinjoin/output?roundId={roundId}", request.ToHttpStringContent()).ConfigureAwait(false);
+			using var response = await TorClient.SendAsync(HttpMethod.Post, $"/api/v{MustardWalletLTCClient.ApiVersion}/ltc/chaumiancoinjoin/output?roundId={roundId}", request.ToHttpStringContent()).ConfigureAwait(false);
 			if (response.StatusCode == HttpStatusCode.Conflict)
 			{
 				return false;