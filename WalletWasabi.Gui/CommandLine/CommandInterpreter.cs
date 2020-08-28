nopara73/WalletWasabi

Initial port to Litecoin
Most functionality works
------------------------
- Litecoin wallet creation
- Sending and receiving Litecoins
- CoinJoining (" mixing ") Litecoins to enhance privacy
- Running with the built-in full node ( litecoind )
- Connecting to your own full Litecoin node
- Running with and without Tor

Notes
-----
- As this is the very first Release , please consider it a beta - there might be Bugs .
- CoinJoins use a small anonymity set for now , which will be gradually increased as we gain more users .
- Check the sha256sums (posted here and on other channels like Reddit) to verify the integrity .
- Unit tests haven't been ported yet . Help is welcome here , mostly trivial fixes .
- The logo is still the Wasabi Wallet Logo , ideas for a Mustard Wallet for Litecoin Logo , anyone ?

Hardware wallet integration
---------------------------
Should work , but currently untested . If you've tested it , and you find that it works with Wasabi but not with Mustard Wallet , please report the issue .

macOS support
-------------
The macOS build is ready as a .zip file and should work , but I haven't tested it because I don't have a macOS release environment to sign and test the Builds on .

WalletWasabi.Gui/CommandLine/CommandInterpreter.cs
@@ -24,13 +24,13 @@ namespace WalletWasabi.Gui.CommandLine
 			}
 
 			OptionSet options = null;
-			var suite = new CommandSet("wassabee")
+			var suite = new CommandSet("mustardwalletltc")
 			{
-				"Usage: wassabee [OPTIONS]+",
-				"Launches Wasabi Wallet.",
+				"Usage: mustardwalletltc [OPTIONS]+",
+				"Launches Mustard Wallet for Litecoin , based on Wasabi Wallet for Bitcoin .",
 				"",
 				{ "h|help", "Displays help page and exit.", x => showHelp = x != null },
-				{ "v|version", "Displays Wasabi version and exit.", x => showVersion = x != null },
+				{ "v|version", "Displays version and exit.", x => showVersion = x != null },
 				"",
 				"Available commands are:",
 				"",
@@ -69,9 +69,9 @@ namespace WalletWasabi.Gui.CommandLine
 
 		private static void ShowVersion()
 		{
-			Console.WriteLine($"Wasabi Client Version: {Constants.ClientVersion}");
+			Console.WriteLine($"Mustard Wallet for Litecoin Client Version: {Constants.ClientVersion}");
 			Console.WriteLine($"Compatible Coordinator Version: {Constants.BackendMajorVersion}");
-			Console.WriteLine($"Compatible Bitcoin Core and Bitcoin Knots Versions: {Constants.BitcoinCoreVersion}");
+			Console.WriteLine($"Compatible Litecoin Core: {Constants.BitcoinCoreVersion}");
 			Console.WriteLine($"Compatible Hardware Wallet Interface Version: {Constants.HwiVersion}");
 		}
 
@@ -79,8 +79,8 @@ namespace WalletWasabi.Gui.CommandLine
 		{
 			ShowVersion();
 			Console.WriteLine();
-			Console.WriteLine("Usage: wassabee [OPTIONS]+");
-			Console.WriteLine("Launches Wasabi Wallet.");
+			Console.WriteLine("Usage: mustardwalletltc [OPTIONS]+");
+			Console.WriteLine("Launches Mustard Wallet for Litecoin , based on Wasabi Wallet for Bitcoin .");
 			Console.WriteLine();
 			Console.WriteLine("Options:");
 			p.WriteOptionDescriptions(Console.Out);

WalletWasabi.Gui/CommandLine/PasswordFinderCommand.cs
@@ -22,7 +22,7 @@ namespace WalletWasabi.Gui.CommandLine
 				"usage: findpassword --wallet:WalletName --language:lang --numbers:[TRUE|FALSE] --symbold:[TRUE|FALSE]",
 				"",
 				"Tries to find typing mistakes in the user password by brute forcing it char by char.",
-				"eg: ./wassabee findpassword --wallet:MyWalletName --numbers:false --symbold:true",
+				"eg: ./mustardwalletltc findpassword --wallet:MyWalletName --numbers:false --symbold:true",
 				"",
 				{ "w|wallet=", "The name of the wallet file.", x => WalletName = x },
 				{ "s|secret=", "You can specify an encrypted secret key instead of wallet. Example of encrypted secret: 6PYTMDmkxQrSv8TK4761tuKrV8yFwPyZDqjJafcGEiLBHiqBV6WviFxJV4", x => EncryptedSecret = Guard.Correct(x) },

MustardWallet/MustardWallet

WalletWasabi.Gui/CommandLine/CommandInterpreter.cs
@@ -29,13 +29,13 @@ namespace WalletWasabi.Gui.CommandLine
 				return true;
 			}
 
-			var suite = new CommandSet("wassabee", Out, Error)
+			var suite = new CommandSet("mustardwalletltc", Out, Error)
 			{
-				"Usage: wassabee [OPTIONS]+",
-				"Launches Wasabi Wallet.",
+				"Usage: mustardwalletltc [OPTIONS]+",
+				"Launches Mustard Wallet for Litecoin , based on Wasabi Wallet for Bitcoin .",
 				"",
 				{ "h|help", "Displays help page and exit.", x => showHelp = x != null },
-				{ "v|version", "Displays Wasabi version and exit.", x => showVersion = x != null },
+				{ "v|version", "Displays version and exit.", x => showVersion = x != null },
 				"",
 				"Available commands are:",
 				"",
@@ -76,9 +76,9 @@ namespace WalletWasabi.Gui.CommandLine
 
 		private void ShowVersion()
 		{
-			Out.WriteLine($"Wasabi Client Version: {Constants.ClientVersion}");
+			Out.WriteLine($"Mustard Wallet for Litecoin Client Version: {Constants.ClientVersion}");
 			Out.WriteLine($"Compatible Coordinator Version: {Constants.ClientSupportBackendVersionText}");
-			Out.WriteLine($"Compatible Bitcoin Core and Bitcoin Knots Versions: {Constants.BitcoinCoreVersion}");
+			Out.WriteLine($"Compatible Litecoin Core Version: {Constants.BitcoinCoreVersion}");
 			Out.WriteLine($"Compatible Hardware Wallet Interface Version: {Constants.HwiVersion}");
 		}
 	}

WalletWasabi.Gui/CommandLine/PasswordFinderCommand.cs
@@ -22,7 +22,7 @@ namespace WalletWasabi.Gui.CommandLine
 				"usage: findpassword --wallet:WalletName --language:lang --numbers:[TRUE|FALSE] --symbols:[TRUE|FALSE]",
 				"",
 				"Tries to find typing mistakes in the user password by brute forcing it char by char.",
-				"eg: ./wassabee findpassword --wallet:MyWalletName --numbers:false --symbols:true",
+				"eg: ./mustardwalletltc findpassword --wallet:MyWalletName --numbers:false --symbols:true",
 				"",
 				{ "w|wallet=", "The name of the wallet file.", x => WalletName = x },
 				{ "s|secret=", "You can specify an encrypted secret key instead of wallet. Example of encrypted secret: 6PYTMDmkxQrSv8TK4761tuKrV8yFwPyZDqjJafcGEiLBHiqBV6WviFxJV4", x => EncryptedSecret = Guard.Correct(x) },